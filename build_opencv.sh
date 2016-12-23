f77a851b24555c6902a7da3e8d25b0f06680985f
Ben John <github@benjohn.de>
2016-12-23 17:06:28 +0100

    Typo
    Expanded cmake command chain...

--- build_opencv.sh
+++ build_opencv.sh
@@ -8,10 +8,14 @@ git checkout 3.2.0
 
 mkdir /opencv/build
 cd /opencv/build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
-  -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF \
+cmake \
+  -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX=/usr/local \
+  -D INSTALL_C_EXAMPLES=OFF \
+  -D INSTALL_PYTHON_EXAMPLES=OFF \
   -D OPENCV_EXTRA_MODULES_PATH=/opencv_contrib/modules \
-  -D BUILD_opencv_java=OFF  -D BUILD_opencv_python=OFF \
+  -D BUILD_opencv_java=OFF \
+  -D BUILD_opencv_python=OFF \
   -D BUILD_EXAMPLES=OFF ..
 
 make -j`nproc`
